9cc4b28ab2ee1b11f474ecf8849d00859ff435fe
Masha Pulnaya <mp1992@yandex.ru>
2023-05-16 16:54:21 +0300

    or

---
+++ index.js
@@ -0,0 +1,36 @@
+const openPopupButton = document.querySelector('#open-popup-button');
+const closePopupButton = document.querySelector('#close-popup-button');
+const editPopup = document.querySelector('#edit-popup');
+const profileTitle = document.querySelector('.profile__title');
+const nameInput = document.querySelector('#name-input');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+const professionInput = document.querySelector('#profession-input');
+const editForm = document.querySelector('#edit-form');
+
+openPopupButton.addEventListener('click', function() {
+    openPopup(editPopup);
+});
+
+closePopupButton.addEventListener('click', function() {
+    closePopup(editPopup);
+});
+
+nameInput.value = profileTitle.textContent;
+professionInput.value = profileSubtitle.textContent;
+
+editForm.addEventListener('submit', function(event) {
+event.preventDefault();
+
+profileTitle.textContent = nameInput.value;
+profileSubtitle.textContent = professionInput.value;
+
+closePopup(editPopup);
+});
+
+function openPopup(popup) {
+  popup.classList.add('popup_is-opened');
+}
+
+function closePopup(popup) {
+    popup.classList.remove('popup_is-opened');
+}
