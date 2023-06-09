42a89164f94c49a3310519be35c0d98877b2c9b1
Masha Pulnaya <mp1992@yandex.ru>
2023-06-09 19:52:05 +0300

    ok

---
+++ scripts/validate.js
@@ -0,0 +1,10 @@
+const form = document.forms.editForm;
+
+const className = {
+    inputsConteiner: 'popup__container',
+    input: 'popup__input',
+    inputInvalid: 'form__input_invalid',
+    error: 'form__error'
+}
+
+console.log(className);
