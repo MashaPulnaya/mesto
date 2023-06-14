b4d77a1074fc618b996155e9b8a92eae53406631
Masha Pulnaya <mp1992@yandex.ru>
2023-06-14 16:54:20 +0300

    f

--- scripts/validate.js
+++ scripts/validate.js
@@ -12,6 +12,20 @@ const hideInputError = (formElement, inputElement) => {
   formError.textContent = '';
 };
 
+const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+    }; 
+
+    const toggleButtonState = (inputList, buttonElement) => {
+        if (hasInvalidInput(inputList)) {
+          buttonElement.classList.add('popup__button_inactive');
+        } else {
+          buttonElement.classList.remove('popup__button_inactive');
+        }
+      }; 
+
 const checkInputValidity = (formElement, inputElement) => {
   if (!inputElement.validity.valid) {
     showInputError(formElement, inputElement, inputElement.validationMessage);
@@ -22,9 +36,13 @@ const checkInputValidity = (formElement, inputElement) => {
 
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__button');
+  toggleButtonState(inputList, buttonElement);
+
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
       checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
     });
   });
 };
